8f79b4ef30d6e2da3b5e0d0de021a63238a0bdd9
Александра Первушова <st088180@student.spbu.ru>
2020-10-20 00:07:28 +0300

    yhf

--- 2020.10.15-Homework-5/Task1/ArrayList.cpp
+++ 2020.10.15-Homework-5/Task1/ArrayList.cpp
@@ -1,21 +1,20 @@
 #include "ArrayList.h"
-void expand()
-{
-	if (count = capacity)
-	{
 
-	}
+ArrayList::~ArrayList()
+{
+	delete[] data;
 }
-void expand(int*& data, int& capacity)
+
+void ArrayList::expand()
 {
 	int* newData = new int[capacity * 2];
 	for (int i = 0; i < capacity; ++i)
 	{
 		newData[i] = data[i];
+		capacity *= 2;
+		delete[] data;
+		data = newData;
 	}
-	capacity *= 2;
-	delete[] data;
-	data = newData;
 }
 
 int ArrayList:: numLength(int number)
@@ -60,3 +59,171 @@ void ArrayList::addNumberToStr(int& index, int number)
 	index += length;
 }
 
+bool ArrayList::add(int element)
+{
+	/*if (count = capacity)
+	{
+		expand();
+	}*/
+	data[count] = element;
+	++count;
+	return true;
+}
+
+bool ArrayList::add(int index, int element)
+{
+	if (count = capacity)
+	{
+		expand();
+	}
+	for (int i = count; i > index; --i)
+	{
+		data[i] = data[i - 1];
+	}
+	data[index] = element;
+	++count;
+	return true;
+}
+
+bool ArrayList::addAll(ArrayList& list)
+{
+	while (count + list.count >= capacity)
+	{
+		expand();
+	}
+	for (int i = 0; i < list.count; ++i)
+	{
+		data[count + i] = list.data[i];
+	}
+	count += list.count;
+	return true;
+}
+
+bool ArrayList::addAll(int index, ArrayList& list)
+{
+	while (count + list.count >= capacity)
+	{
+		expand();
+	}
+	for (int i = count - 1; i >= index; --i)
+	{
+		data[i + list.count] = data[i];
+	}
+	for (int i = 0; i < list.count; ++i)
+	{
+		data[i + index] = list.data[i];
+	}
+	count += list.count;
+	return true;
+}
+
+void ArrayList::clear()
+{
+	count = 0;
+}
+
+bool ArrayList::contains(int element)
+{
+	for (int i = 0; i < count; ++i)
+	{
+		if (data[i] == element)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+int ArrayList::get(int index)
+{
+	if (index <= count)
+	{
+		return data[index];
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+bool ArrayList::set(int index, int element)
+{
+	if (index < count)
+	{
+		data[index] = element;
+		return true;
+	}
+	return false;
+}
+
+int ArrayList::indexOf(int element)
+{
+	for (int i = 0; i < count; ++i)
+	{
+		if (data[i] == element)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+bool ArrayList::isEmpty()
+{
+	if (count = 0)
+	{
+		return true;
+	}
+	return false;
+}
+
+char* ArrayList::toString()
+{
+	int length = 7 + numLength(count) + numLength(capacity) + (count - 1) * 2;
+	for (int i = 0; i < count; ++i)
+	{
+		length += numLength(data[i]);
+	}
+	str = new char[length];
+	int index = 0;
+	addSymbolToStr(index, '[');
+	addNumberToStr(index, count);
+	addSymbolToStr(index, '/');
+	addNumberToStr(index, capacity);
+	addSymbolToStr(index, ']');
+	addSymbolToStr(index, ' ');
+	addSymbolToStr(index, '{');
+	for (int i = 0; i < count - 1; ++i)
+	{
+		addNumberToStr(index, data[i]);
+		addSymbolToStr(index, ',');
+		addSymbolToStr(index, ' ');
+	}
+	addNumberToStr(index, data[count - 1]);
+	addSymbolToStr(index, '}');
+	addSymbolToStr(index, '\0');
+	return str;
+}
+
+bool ArrayList::remove(int index)
+{
+	for (int i = index; i < count - 1; ++i)
+	{
+		data[i] = data[i + 1];
+	}
+	--count;
+	return true;
+}
+
+bool ArrayList::swap(int index1, int index2)
+{
+	int temp = data[index1];
+	data[index1] = data[index2];
+	data[index2] = temp;
+	return true;
+}
+
+int ArrayList::length()
+{
+	return count;
+}
--- 2020.10.15-Homework-5/Task1/Source.cpp
+++ 2020.10.15-Homework-5/Task1/Source.cpp
@@ -1,10 +1,61 @@
 #include <iostream>
 #include "ArrayList.h"
+#include <random>
 
 using namespace std;
 
 int main()
 {
+	int k = 10;
+	cout << k;
+	ArrayList a;
+	cout << a.get(2);
+	a.add(1);
+	cout << a.get(0);
+	for (int i = 0; i < k; ++i)
+	{
+		a.add(rand() % 90 + 10);
+	}
+	cout << a.toString();
+
+	for (int i = 0; i < k; ++i)
+	{
+		a.add(rand() % 90 - 99);
+	}
+	cout << a.toString();
+
+	int min = 0;
+	int max = 0;
+	for (int i = 0; i < a.length(); ++i)
+	{
+		if (a.get(i) < a.get(min))
+		{
+			min = i;
+		}
+	}
+	for (int i = 0; i < a.length(); ++i)
+	{
+		if (a.get(i) >= a.get(max))
+		{
+			max = i;
+		}
+	}
+	a.swap(min, max);
+
+	for (int i = 0; i < a.length(); ++i)
+	{
+		int j = rand() % (i + 1);
+		a.swap(i, j);
+	}
+	cout << a.toString();
+
+	for (int i = 0; i < a.length(); ++i)
+	{
+		if (a.get(i) < 0)
+		{
+			a.set(i, 0);
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
