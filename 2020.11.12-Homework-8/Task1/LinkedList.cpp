4c0e93378a61c9d6c55a00de8af8fcc161fad36d
Александра Первушова <st088180@student.spbu.ru>
2020-11-23 16:56:22 +0300

    Односвязный список без swap

---
+++ 2020.11.12-Homework-8/Task1/LinkedList.cpp
@@ -0,0 +1,326 @@
+﻿#include "LinkedList.h"
+
+LinkedList::LinkedList(const LinkedList& list)
+{
+	tail = head = nullptr;
+	count = 0;
+
+	Node* temp = list.head;
+	while (temp != nullptr)
+	{
+		addToTail(temp->data);
+		temp = temp->next;
+	}
+}
+
+LinkedList::~LinkedList()
+{
+	Node* temp = head;
+	while (temp != nullptr)
+	{
+		Node* node = temp;
+		temp = temp->next;
+		delete node;
+	}
+}
+
+bool LinkedList::indexValid(int index)
+{
+	return index >= 0 && index < count;
+}
+
+int LinkedList::length()
+{
+	return count;
+}
+
+bool LinkedList::addToHead(int element)
+{
+	if (head == nullptr)
+	{
+		head = tail = new Node(element);
+	}
+	else
+	{
+		head = new Node(element, head);
+	}
+	++count;
+	return true;
+}
+
+bool LinkedList::addToTail(int element)
+{
+	if (tail == nullptr)
+	{
+		head = tail = new Node(element);
+	}
+	else
+	{
+		tail->next = new Node(element);
+		tail = tail->next;
+	}
+	++count;
+	return true;
+}
+
+bool LinkedList::add(int index, int element)
+{
+	if (!indexValid(index))
+	{
+		return false;
+	}
+	if (index == 0)
+	{
+		return addToHead(index);
+	}
+	if (index == count)
+	{
+		return addToTail(index);
+	}
+	Node* temp = head;
+	while (index > 1)
+	{
+		temp = temp->next;
+		--index;
+	}
+	temp->next = new Node(element, temp->next);
+	++count;
+	return true;
+}
+
+int LinkedList::get(int index)
+{
+	if (!indexValid(index))
+	{
+		return -1;
+	}
+	if (index == 0)
+	{
+		return head->data;
+	}
+	if (index == count - 1)
+	{
+		return tail->data;
+	}
+	Node* temp = head;
+	while (index > 0)
+	{
+		temp = temp->next;
+		--index;
+	}
+	return temp->data;
+}
+
+bool LinkedList::set(int index, int element)
+{
+	if (!indexValid(index))
+	{
+		return false;
+	}
+	if (index == 0)
+	{
+		head->data = element;
+	}
+	else if (index == count - 1)
+	{
+		tail->data = element;
+	}
+	else
+	{
+		Node* temp = head;
+		while (index > 0)
+		{
+			temp = temp->next;
+			--index;
+		}
+		temp->data = element;
+	}
+	return true;
+}
+
+int& LinkedList::operator[](int index)
+{
+	if (head == nullptr)
+	{
+		addToHead(0);
+		return head->data;
+	}
+	if (index <= 0)
+	{
+		return head->data;
+	}
+	if (index >= count)
+	{
+		addToTail(0);
+		return tail->data;
+	}
+	if (index == count - 1)
+	{
+		return tail->data;
+	}
+	Node* temp = head;
+	while (index > 0)
+	{
+		temp = temp->next;
+		--index;
+	}
+	return temp->data;
+}
+
+void LinkedList::operator+=(int element)
+{
+	addToTail(element);
+}
+
+int LinkedList::extractHead()
+{
+	if (head == nullptr)
+	{
+		return -1;
+	}
+	int result = head->data;
+	if (head->next == nullptr)
+	{
+		delete head;
+		head = nullptr;
+		tail = nullptr;
+		--count;
+		return result;
+	}
+	Node* node = head->next;
+	delete head;
+	head = node;
+	--count;
+	return result;
+}
+
+int LinkedList::extractTail()
+{
+	if (head == nullptr)
+	{
+		return -1;
+	}
+	int result = tail->data;
+	if (head->next == nullptr)
+	{
+		delete head;
+		head = nullptr;
+		tail = nullptr;
+		--count;
+		return result;
+	}
+	Node* node = head;
+	while (node->next != nullptr && node->next->next != nullptr)
+	{
+		node = node->next;
+	}
+	delete node->next;
+	tail = node;
+	tail->next = nullptr;
+	--count;
+	return result;
+}
+
+int LinkedList::extract(int index)
+{
+	if (head == nullptr || !(indexValid(index)))
+	{
+		return -1;
+	}
+	if (index == 0)
+	{
+		return extractHead();
+	}
+	if (index == count - 1)
+	{
+		return extractTail();
+	}
+	Node* node = head;
+	int num = 0;
+	while (num != index - 1)
+	{
+		node = node->next;
+		++num;
+	}
+	Node* temp = node->next;
+	int result = temp->data;
+	node->next = node->next->next;
+	delete temp;
+	--count;
+	return result;
+}
+
+void LinkedList::operator-=(int index)
+{
+	extract(index);
+}
+
+LinkedList& LinkedList::operator=(LinkedList list)
+{
+	Node* temp1 = head;
+	for (int i = 0; i < count; ++i)
+	{
+		Node* node = temp1;
+		temp1 = temp1->next;
+		delete node;
+	}
+	Node* temp2 = list.head;
+	while (temp2 != nullptr)
+	{
+		addToTail(temp2->data);
+		temp2 = temp2->next;
+	}
+
+	return *this;
+}
+
+int LinkedList::indexOf(int element)
+{
+	if (head == nullptr)
+	{
+		return -1;
+	}
+	Node* node = head;
+	int index = 0;
+	while (node != nullptr)
+	{
+		if (node->data == element)
+		{
+			return index;
+		}
+		node = node->next;
+		++index;
+	}
+	return -1;
+}
+
+bool LinkedList::contains(int element)
+{
+	return ((indexOf(element) != -1) ? true : false);
+}
+
+std::ostream& operator<<(std::ostream& stream, const LinkedList list)
+{
+	stream << "[" << list.count << "]{";
+	if (list.head == nullptr)
+	{
+		stream << "EMPTY";
+	}
+	else
+	{
+		Node* temp = list.head;
+		while (temp != nullptr)
+		{
+			stream << temp->data;
+			if (temp->next != nullptr)
+			{
+				stream << ", ";
+			}
+			temp = temp->next; //ïåðåõîäèì íà ñëåäóþùèé ýëåìåíò
+		}
+	}
+	stream << "}";
+
+	return stream;
+}
+
---
+++ 2020.11.12-Homework-8/Task1/Source.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include "LinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	LinkedList list1;
+	cout << "list1: " << list1 << endl;
+	cout << "list[3] = " << list1.get(3) << endl;
+	list1.addToHead(45);
+	cout << "list1: " << list1 << endl;
+	list1.extractHead();
+	cout << "list1: " << list1 << endl;
+	list1.addToTail(54);
+	cout << "list1: " << list1 << endl;
+	list1.extractTail();
+	cout << "list1: " << list1 << endl;
+	list1.add(405, 504);
+	cout << "list1: " << list1 << endl;
+    list1[405] = 504;
+	cout << "list1: " << list1 << endl;
+	list1.extract(0);
+	list1 += 9;
+	cout << "list1: " << list1 << endl;
+	list1 -= 0;
+	cout << "list1: " << list1 << endl;
+
+	list1.addToHead(4);
+	cout << "list1: " << list1 << endl;
+    list1 += 5;
+	cout << "list1: " << list1 << endl;
+
+	LinkedList list2;
+	cout << "list2: " << list2 << endl;
+	list2 = list1;
+	cout << "list2: " << list2 << endl;
+	list2.add(1, 0);
+	cout << "list2: " << list2 << endl;
+	list2.add(3, 0);
+	cout << "list2: " << list2 << endl;
+	list2.set(3, 0);
+	cout << "list2: " << list2 << endl;
+	list2.addToTail(4);
+	cout << "list2: " << list2 << endl;
+	list2.extractHead();
+	cout << "list2: " << list2 << endl;
+	list2.set(0, 9);
+	cout << "list2: " << list2 << endl;
+	list2.extractTail();
+	cout << "list2: " << list2 << endl;
+	list2.set(4, 9);
+	cout << "list2: " << list2 << endl;
+	list2[2] = 4;
+	cout << "list2: " << list2 << endl;
+	list1 = list2;
+	cout << "list1: " << list1 << endl;
+	return 0;
+}
+
+
+
+
+
+/*продемонстрировать работу всех функций
+показать, что все функции и операторы корректно работают:
+1). С пустым списком.
+2). Со списком из 1 элемента.
+3). Со списком из нескольких элементов.
+*/
