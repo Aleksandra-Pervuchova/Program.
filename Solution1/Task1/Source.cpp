c3dbe53801ac95d9dc129d2b2b963ce3a793793d
Александра Первушова <st088180@student.spbu.ru>
2020-12-03 12:21:22 +0300

    чувствую себя пеньком

--- Solution1/Task1/Source.cpp
+++ Solution1/Task1/Source.cpp
@@ -5,14 +5,13 @@ using namespace std;
 
 double myFunction(float x, int n)
 {
-	double result = 0;
+	double result = x;
 	int j = 1;	
 	double d = x;
-	result = x;
 	while (true)
 	{
 		j += 2;
-		d *= (-1) * x * x /j/(j-1);
+		d *= ( -1 ) * x * x / j / (j-1);
 		result += d;
 
 		if (result * (10 ^ n) - floor(result * (10 ^ n)) > 0)
@@ -25,11 +24,11 @@ double myFunction(float x, int n)
 
 int main()
 {
+	setprecision(9);
 	double x = 0;
 	int n = 0;
 	cin >> x >> n;
 	cout << sin(x) << endl;
 	cout << myFunction(x, n) << endl;
 	return 0;
-
-}/*fabs(result * (10 ^ n)) - floor(fabs(result * (10 ^ n))) > 0*/
+}
